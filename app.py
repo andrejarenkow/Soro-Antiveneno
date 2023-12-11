dfccdd0894258a57fd342c4cbd6c4fba0b0a6c74
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-11 15:38:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 col1, col2, col3 = st.columns([1,4,1])
 
 col1.image('https://github.com/andrejarenkow/PainelOvitrampas/blob/main/logo_cevs%20(1).png?raw=true', width=200)
-col2.title('Soro Antiveneno')
+col2.title('Buscador de soros antiveneno')
 col3.image('https://github.com/andrejarenkow/PainelOvitrampas/blob/main/logo_estado%20(3).png?raw=true', width=300)
 
 #dicionario soros
