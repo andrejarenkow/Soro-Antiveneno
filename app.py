c9019942599404fa2ff7080a7d564036ca7f9497
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2023-12-13 15:59:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ try:
     
     map_fig = px.choropleth_mapbox(municipios_soro_destino, geojson=municipios_soro_destino.geometry,
                               locations=municipios_soro_destino.index, color='Legenda',
-                              center ={'lat':municipios_soro_destino['Latitude'].y.values[0], 'lon':municipios_soro_destino.['Longitude'].x.values[0]}
+                              center = {'lat': municipios_soro_destino['Latitude'].values[0], 'lon': municipios_soro_destino['Longitude'].values[0]}
                               #center ={'lat':municipios_soro_destino.geometry.centroid.y.values[0], 'lon':municipios_soro_destino.geometry.centroid.x.values[0]},
                               zoom=7.5,
                               mapbox_style="open-street-map",
