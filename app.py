df48096be8eb0510fe3e6f460a1d3e5bcedfe073
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-14 11:07:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,8 +92,8 @@ with col5:
     municipios_soro['Legenda'] = 'Origem'
     
     municipios_soro = municipios_soro.dropna()
-    
-    mun_destino = municipios_soro.dropna().reset_index(drop=True)['Município destino'].values[0]
+    municipios_soro
+    #mun_destino = municipios_soro.dropna().reset_index(drop=True)['Município destino'].values[0]
         
     filtro_destino = (dados_geral['soro'] == soro)&(dados_geral['Origin'] == mun_destino)
     municipio_destino = dados_geral[filtro_destino].dropna()
