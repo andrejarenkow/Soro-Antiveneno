6d1803af8f3eee69c8b13dccbb528faba90b3907
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2023-12-19 10:41:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -149,3 +149,10 @@ with col5:
                              lon="Longitude_destino", 
                              hover_name="Destination")
         st.plotly_chart(fig)
+        fig.update_geos(
+            projection_type="open street map",
+            center=dict(lat=-29, lon=-53),  # Ponto central do mapa (latitude, longitude)
+            scope="brasil",  # Escopo geográfico ("world", "usa", "europe", "asia", etc.)
+            showland=True,
+            landcolor="rgba(255, 255, 255, 0)",  # Cor da terra (transparente)
+)
