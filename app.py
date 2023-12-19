8b52dd89dec71eae5f86b0b51896ff771f33aeb4
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2023-12-19 09:46:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -141,6 +141,8 @@ with col5:
                 st.write('**ATENÇÃO**: ligue para o local para fazer a confirmação da disponibilidade do soro.')
     except:
         st.write('')
+        dados_geral.Latitude_destino.y = dados_geral.Latitude_destino.y.astype('float')
+        dados_geral.Longitude_destino.x = dados_geral.Longitude_destino.x.astype('float')
         fig = px.scatter_geo(dados_geral.drop_duplicates(['Destination']),
                     lat=dados_geral.drop_duplicates.Latitude_destino.y,
                     lon=dados_geral.drop_duplicates.Longitude_destino.x,
