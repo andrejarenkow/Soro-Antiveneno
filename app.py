8c984e77cbf3e51af494d40f2b3e2cfff04af9fd
barbara-pietoso <153303973+barbara-pietoso@users.noreply.github.com>
2023-12-19 11:03:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,8 @@ with col5:
         fig = px.scatter_mapbox(pontos,
                                 lat="Latitude_destino", 
                                 lon="Longitude_destino", 
-                                hover_name="Destination")
+                                hover_name="Destination"
+                                zoom=5.5)
         fig.update_layout(margin=go.layout.Margin(l=10, r=10, t=10, b=10),paper_bgcolor='rgba(0,0,0,0)',
                           mapbox_accesstoken= 'pk.eyJ1IjoiYW5kcmUtamFyZW5rb3ciLCJhIjoiY2xkdzZ2eDdxMDRmMzN1bnV6MnlpNnNweSJ9.4_9fi6bcTxgy5mGaTmE4Pw')
         st.plotly_chart(fig)
