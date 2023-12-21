64927117629ae28dfe7cb3d955cc22dd19815421
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-21 11:55:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -227,7 +227,7 @@ with tab2:
     
 with tab3:
     st.header("Metodologia")
-    texto_metodologia = """    
+    texto_metodologia_1 = """    
         
     A metodologia utilizada na realização e coleta de informações para originar o “Buscador de Soros Antivenenos” foi desenvolvida no Software QGIS, a partir de um processo chamado de Network Analysis, com objetivo de adquirir as distâncias dos municípios do Rio Grande do Sul, ao ponto focal de soro antiveneno mais próximo. Para a idealização do projeto foram utilizados dados shapefile (linhas, pontos e polígonos), ferramentas e plugins do programa QGIS.
     
@@ -247,15 +247,15 @@ with tab3:
     Como nem todos Pontos Focais têm em seu estoque todos 6 Soros Antivenenos disponibilizados, o geoprocessamento foi realizado seis vezes, uma vez para cada Soro Antiveneno. Uma vez para Pontos Focais com SAB, uma vez para Pontos Focais com SAC, e assim por diante. (Figura 3)
     
     ![Figura 3](https://github.com/andrejarenkow/Soro-Antiveneno/blob/main/imagens_metodologia/figura3.png?raw=true) 
-    <img src="https://github.com/andrejarenkow/Soro-Antiveneno/blob/main/imagens_metodologia/figura3.png?raw=true" width="200" height="200" />
     
     1- Soro antiveneno SAC | 2- Soro antiveneno SAE | 3- Soro antiveneno Saar |
     4- Soro antiveneno SAEsc | 5- Soro antiveneno SAB | 6- Soro antiveneno SALon
     
     Como produto final da metodologia aplicada foram obtidas tabelas dos municípios do Rio Grande do Sul e qual Ponto Focal se localiza mais próximo do mesmo, com distância em km. Foi obtida uma tabela para cada Soro Antiveneno (SAB, SAC, SAE, SAEsc, SALon e Saar). (Figura 4)
+    """
     
-    ![Figura 4](https://github.com/andrejarenkow/Soro-Antiveneno/blob/main/imagens_metodologia/figura4.png?raw=true "Figura 4")
-    
+    texto_metodologia_2 = 
+    """
     Exemplo de tabela gerada após o geoprocessamento dos dados.
     Tabela para Soro Antiveneno SAB.
     
@@ -265,4 +265,6 @@ with tab3:
     
     """
 
-    st.markdown(texto_metodologia)
+    st.markdown(texto_metodologia_1)
+    st.image('https://github.com/andrejarenkow/Soro-Antiveneno/blob/main/imagens_metodologia/figura4.png?raw=true', width=500 )
+    st.markdown(texto_metodologia_2)
