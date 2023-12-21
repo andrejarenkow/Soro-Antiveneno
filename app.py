ad07d6871bd2d2a72a455c07798db9f941c58d0c
carlonissinen <153944659+carlonissinen@users.noreply.github.com>
2023-12-21 10:23:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -225,9 +225,7 @@ A metodologia utilizada na realização e coleta de informações para originar 
 
 Inicialmente foram introduzidos e espacializados os Pontos Focais (locais onde os soros antivenenos se encontram) no software, de forma que fosse possível a visualização dos mesmos no território do estado do Rio Grande do Sul. Estes Pontos Focais foram adquiridos por meio de uma tabela disponibilizada pelo Centro de Informação Toxicológica (CIT). Juntos dos Pontos Focais foram adicionados pontos das sedes municipais do Rio Grande do Sul (estes pontos foram identificados a partir do centro urbano dos municípios), um shapefile das rodovias do RS e um shapefile de polígonos do estado do RS (Figura 1). 
 
-![Figura 1](/imagens_metodologia/figura1.png "Figura 1")
-
-![Image]("F:\VIGIAR_VIGISOLO\Carlo\Projeto Municípios Antivenenos\Imagens metodologia\figura1.png" "Figura 1")
+![Image](/imagens_metodologia/figura1.png "Figura 1")
 
 Pontos Focais de Soro Antiveneno, em vermelho. Sedes Municipais, em azul. Rodovias do RS, em marrom.
 
