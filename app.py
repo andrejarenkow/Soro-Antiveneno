f938559bd4719107405221fa94532c0d08e42f1f
carlonissinen <153944659+carlonissinen@users.noreply.github.com>
2023-12-21 10:08:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -217,11 +217,7 @@ No Rio Grande do Sul há diversas espécies de escorpiões, sendo que a maioria 
 Existem diversas lagartas urticantes no Rio Grande do Sul, mas a _Lonomia obliqua_, conhecida como taturana, é a única que pode causar acidentes graves com a necessidade do uso de soro antilonômico. 
 
 **_Lonomia obliqua_**: conhecida como taturana, o seu ciclo de vida é de, em média, 150 dias. A fase de lagarta, que representa perigo devido à presença das cerdas urticantes, tem duração de 60 dias. Durante esta fase da vida, as lagartas permanecem agregadas em troncos de árvores durante o dia e, à noite, sobem para as copas para se alimentar de folhas. Por esta característica de permanecerem agregadas, quando um acidente ocorre, o paciente entra em contato com vários animais ao mesmo tempo. O maior perigo deste acidente é a sua toxina, que pode levar à insuficiência renal aguda.
-"""
 
-st.markdown(texto_sobre)
-
-texto_metodologia = """
 
 ### METODOLOGIA
 
@@ -229,5 +225,12 @@ A metodologia utilizada na realização e coleta de informações para originar 
 
 Inicialmente foram introduzidos e espacializados os Pontos Focais (locais onde os soros antivenenos se encontram) no software, de forma que fosse possível a visualização dos mesmos no território do estado do Rio Grande do Sul. Estes Pontos Focais foram adquiridos por meio de uma tabela disponibilizada pelo Centro de Informação Toxicológica (CIT). Juntos dos Pontos Focais foram adicionados pontos das sedes municipais do Rio Grande do Sul (estes pontos foram identificados a partir do centro urbano dos municípios), um shapefile das rodovias do RS e um shapefile de polígonos do estado do RS (Figura1). 
 
-st.markdown(texto_metodologia)
+[alt text](https://github.com/andrejarenkow/Soro-Antiveneno/blob/main/imagens_metodologia/figura1.png)
+Figura 1
+Pontos Focais de Soro Antiveneno, em vermelho. Sedes Municipais, em azul. Rodovias do RS, em marrom.
 
+
+
+"""
+
+st.markdown(texto_sobre)
